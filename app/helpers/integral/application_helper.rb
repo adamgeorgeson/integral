bc97170a591b7b42cb69bc1e422823925f828c6c
Patrick Lindsay <ptricklindsay@gmail.com>
2017-06-07 12:51:27 +0100

    GTM: Update google_tag_manager method

--- app/helpers/integral/application_helper.rb
+++ app/helpers/integral/application_helper.rb
@@ -17,21 +17,28 @@ module Integral
     end
 
     # TODO: Could look to move this out into seperate helper class
-    def google_tag_manager
+    def google_tag_manager(type=:script)
       gtm_id = Settings.google_tag_manager_id
       return if gtm_id.blank? || !Rails.env.production?
 
-      snippet = <<-HTML
-       <!-- Google Tag Manager -->
-       <noscript><iframe src="//www.googletagmanager.com/ns.html?id=#{gtm_id}"
-       height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-       <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                                                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                                                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                                                            '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-       })(window,document,'script','dataLayer','#{gtm_id}');</script>
-       <!-- End Google Tag Manager -->
-      HTML
+      if type == :script
+        snippet = <<-HTML
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','#{gtm_id}');</script>
+        <!-- End Google Tag Manager -->
+        HTML
+      else
+        snippet = <<-HTML
+        <!-- Google Tag Manager -->
+        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=#{gtm_id}"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager -->
+        HTML
+      end
 
       snippet.html_safe
     end
